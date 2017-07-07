6be4ae097ec018b07e22754dc096fcdb13183f85
Chris Kontomitros <chkon2009@yahoo.gr>
2017-07-08 00:50:28 +0300

    add_node

---
+++ server.js
@@ -0,0 +1,35 @@
+var http = require('http'),
+    fs=require('fs'),
+    path=require('path'),
+    host='127.0.0.1',
+    port='9000';
+var mimes={
+    ".html" : "text/html",
+    ".css"  : "text/css",
+    ".js"   : "text/javascript",
+    ".gif"  : "text/gif",
+    ".jpg"  : "image/jpg"}
+var server =http.createServer(function(req,res){
+    var filepath=(req.url==='/')?('./fronpage.html'):('.'+req.url);
+    var contentType = mimes[path.extname(filepath)];
+    fs.exists(filepath,function(file_exists){
+	if(file_exists){
+	    fs.readFile(filepath,function(error,content){
+		if(error){
+		    res.writeHead(500);
+		    res.end
+		}
+		else{
+		    res.writeHead(200,{'Content-Type':contentType});
+		    res.end(content,'utf-8');
+		}
+	    })
+	}
+	else{
+	    res.writeHead(404);
+	    res.end("sorry file not found")
+	}
+    })
+}).listen(port,host,function(){
+    console.log('Server Running on localhost');
+})
